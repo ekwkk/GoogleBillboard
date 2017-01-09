f73e7253bcb415c87fc6e42027eda2241dfd6f33
ekwkk <ericakong0331@gmail.com>
2017-01-09 13:03:33 -0800

    final commit

--- GoogleBillboard.pde
+++ GoogleBillboard.pde
@@ -1,50 +1,34 @@
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059"; 
 
 public void setup()  
-{          
-    String digits = e.substring(2,12);
-    double dNum = Double.parseDouble(digits);
-    boolean primeNumber = isPrime(dNum);
-    System.out.println(isPrime(7427466391.0));
-	// while (!primeNumber)
-	// {
-	// 	for (int i = 1; i < e.length()-13; i++)
-	// 	{
-	// 		digits = e.substring(2+i, 12+i);
-	// 		dNum = Double.parseDouble(digits);
-	// 		primeNumber = isPrime(dNum);
-	// 		System.out.println(dNum);
-	// 	}
-	// }
-
-	// System.out.println(dNum);
-}  
-
-
-
-
+{
+  double d = 0.0; 
+  
+  for (int i = 2; i < e.length()-2; i++)
+  {
+   d = Double.parseDouble(e.substring(i, i+10));
+   if (isPrime(d))
+     break;
+  }
+  
+  System.out.println(d);
+  
+}
 
 public void draw()  
 {   
-	// no use
+  // no use
 }  
 
+public boolean isPrime(double num)
+{
+  if (num < 2.0)
+    return false;
+  for (int i = 2; i <= Math.sqrt(num); i++)
+    if (num % i == 0)
+    {
 
-
-
-
-public boolean isPrime(double dNum)  
-{   
-	if (dNum > 1)
-	{
-		for (int i = 2; i <= Math.sqrt(dNum); i++)
-	    {
-	      if (dNum%i == 0)
-	        return false;
-	    }
-	}
-	else if (dNum <= 1)
-		return false;
-  
-  return true; 
-} 
+      return false;
+    }
+  return true;
+}
--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -15,48 +15,39 @@ import java.io.IOException;
 public class GoogleBillboard extends PApplet {
 
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059"; 
-public boolean myBool = false;
+
 public void setup()  
-{            
-     noLoop();  
-     String digits = e.substring(2,12);
-     double dNum = Double.parseDouble(digits);
-     myBool = isPrime(dNum);
-}  
+{
+  double d = 0.0f; 
+  
+  for (int i = 2; i < e.length()-2; i++)
+  {
+   d = Double.parseDouble(e.substring(i, i+10));
+   if (isPrime(d))
+     break;
+  }
+  
+  System.out.println(d);
+  
+}
+
 public void draw()  
 {   
-	String digits = e.substring(2,12);
-	double dNum = Double.parseDouble(digits);
-	System.out.println(e.length());
-	while (myBool == false)
-	{
-		for (int i = 1; i < e.length()-13; i++)
-		{
-			digits = e.substring(2+i, 12+i);
-			dNum = Double.parseDouble(digits);
-			myBool = isPrime(dNum);
-			System.out.println(myBool);
-		}
-	}
-
-	if (myBool)
-		System.out.println(dNum);
+  // no use
 }  
-public boolean isPrime(double dNum)  
-{   
-	if (dNum > 1)
-	{
-		for (int i = 2; i <= Math.sqrt(dNum); i++)
-	    {
-	      if (dNum%i == 0)
-	        return false;
-	    }
-	}
-	else if (dNum <= 1)
-		return false;
-  
-  return true; 
-} 
+
+public boolean isPrime(double num)
+{
+  if (num < 2.0f)
+    return false;
+  for (int i = 2; i <= Math.sqrt(num); i++)
+    if (num % i == 0)
+    {
+
+      return false;
+    }
+  return true;
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "GoogleBillboard" };
     if (passedArgs != null) {
